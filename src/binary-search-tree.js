03ca5a62bca7cf313caf8dcf8e04ef97b09650d2
predmaxim <predmaxim@ya.ru>
2022-10-17 09:43:59 +0500

    feat: add code

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -7,42 +7,115 @@ const { NotImplementedError } = require('../extensions/index.js');
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
 
+  // return root node of the tree
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.root = { data: null, left: null, right: null }
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  // add node with data to the tree
+  add(data) {
+    const _node = { data: data, left: null, right: null }
+    this.root = addWithin(this.root, data)
+
+    function addWithin(node, data) {
+      if (!node) return _node
+      if (node.data === data) return node
+
+      data < node.data
+        ? node.left = addWithin(node.left, data)
+        : node.right = addWithin(node.right, data)
+        
+      return node
+    }
+
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  // returns true if node with the data exists in the tree and false otherwise 
+  has(data) {
+    return searchWithin(this.root, data)
+
+    function searchWithin(node, data) {
+      if (!node) return false
+      if (node.data === data) return true
+      // if (data === null) return 'root'
+
+      return data < node.data
+        ? searchWithin(node.left, data)
+        : searchWithin(node.right, data)
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  // returns node with the data if node with the data exists in the tree and null otherwise
+  find(data) {
+
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  // removes node with the data from the tree if node with the data exists
+  remove(data) {
+    this.root = removeNode(this.root, data)
+
+    function removeNode(node, data) {
+      if (!node || !node.left && !node.left) return null
+      if (data < node.data) node.left = addWithin(node.left, data)
+      if (data > node.data) node.right = addWithin(node.right, data)
+      if (!node.left) node = node.right
+      if (!node.right) node = node.left
+
+      let minFromRight = node.right
+
+      while (minFromRight.left) {
+        minFromRight = minFromRight.left
+      }
+
+      node.data = minFromRight.data
+      node.right = removeNode(node.right, minFromRight.data)
+
+      return node
+    }
   }
 
+  // returns minimal value stored in the tree (or null if tree has no nodes)
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.root) return
+
+    let node = this.root
+
+    while (node.left) {
+      node = node.left
+    }
+
+    return node.data
   }
 
+  // returns maximal value stored in the tree (or null if tree has no nodes)
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.root) return
+
+    let node = this.root
+
+    while (node.right) {
+      node = node.right
+    }
+
+    return node.data
   }
 }
+
+
+const tree = new BinarySearchTree();
+tree.add(2);
+tree.add(7);
+tree.add(1);
+tree.add(8);
+tree.add(4);
+tree.add(32);
+tree.add(12);
+tree.add(14);
+
 
 module.exports = {
   BinarySearchTree
